b6922ccbd45f54c8a506f97dd2a5cbe1b0b18e8a
QuentinRdl <quentinradlo.pro@gmail.com>
2024-05-06 22:41:14 +0200

    Connexion presque fini manque plus que la redirection plus qq petits elements peut etre a tester  Changes to be committed: 	modified:   docker-php/app/gazette/php/connexion.php

--- docker-php/app/gazette/php/connexion.php
+++ docker-php/app/gazette/php/connexion.php
@@ -41,7 +41,7 @@ function affFormulaireConnexion(?array $err): void {
                 '<h2>Formulaire de connexion</h2>',
                 '<p>Pour vous authentifier, remplissez le formulaire ci-dessous.</p>';
     
-    if (is_array($err)) {
+    if (is_array($err) && count($err) > 0) {
         echo    '<div class="erreur">Les erreurs suivantes ont été relevées lors de votre inscription :',
                     '<ul>';
         foreach ($err as $e) {
@@ -104,70 +104,32 @@ function traitementConnexion(): array {
     // ouverture de la connexion à la base
     $bd = bdConnect();
 
-    // protection des entrées
-    $pseudo2 = mysqli_real_escape_string($bd, $pseudo); // fait par principe, mais inutile ici car on a déjà vérifié que le pseudo
-                       // ne contenait que des caractères alphanumériques
-    // calcul du hash du mot de passe pour enregistrement dans la base.
-    
-    $passe = password_hash($_POST['password'], PASSWORD_DEFAULT);
-    $passe = mysqli_real_escape_string($bd, $passe);
-
-    echo $_POST['password'];
-    echo $pseudo2, $passe;
-    
     // Vérification des données de connexion
-$pseudo = mysqli_real_escape_string($bd, $_POST['pseudo']);
+    $pseudo = mysqli_real_escape_string($bd, $_POST['pseudo']);
 
     // Exécution de la requête pour récupérer les informations de l'utilisateur
-    $sql = "SELECT utPseudo, utPasse FROM utilisateur WHERE utPseudo = '$pseudo'";
+    $sql = "SELECT utPseudo, utPasse, utRedacteur FROM utilisateur WHERE utPseudo = '$pseudo'";
     $resultat = bdSendRequest($bd, $sql);
 
     // Vérification si un utilisateur correspondant au pseudo existe dans la base de données
     if(mysqli_num_rows($resultat) == 1) {
         $donneesUtilisateur = mysqli_fetch_assoc($resultat);
-        
+        $reussite=0;
         // Vérification du mot de passe hashé
         if(password_verify($_POST['password'], $donneesUtilisateur['utPasse'])) {
             // Mot de passe correct, connexion réussie
-            echo "Connexion réussie.";
-            // Vous pouvez rediriger l'utilisateur vers la page d'accueil ou effectuer d'autres actions nécessaires après la connexion.
+            $reussite=1;
         } else {
-            // Mot de passe incorrect
-            echo "Mot de passe incorrect.";
+            // echo "Mot de passe incorrect.";
+            $erreurs[] = 'Mot de passe incorrect';
         }
     } else {
-        // Aucun utilisateur correspondant au pseudo donné
-        echo "Aucun utilisateur trouvé avec ce pseudo.";
+        // echo "Aucun utilisateur trouvé avec ce pseudo.";
+        $erreurs[] = 'Aucun utilisateur trouvé avec ce pseudo';
     }
 
-        // Libération de la mémoire associée au résultat de la requête
-        mysqli_free_result($resultat);
-
-
-
-    /*
-    $sql = "SELECT utRedacteur, utEmail FROM utilisateur WHERE utPseudo = '$pseudo2' AND utPasse = '$passe'";
-    
-    $res = bdSendRequest($bd, $sql);
-    $utRedacteur = 0;
-    while($tab = mysqli_fetch_assoc($res)) {
-        $utRedacteur = $tab['utRedacteur']; // Récupérer le statut redacteur de l'user
-         /*
-        if(empty($tab['utEmail'])) {
-            $erreurs[] = 'Utilisateur inconnu ou mot de passe incorrect !';
-        }
-        
-        if(!($tab['utPseudo']==$pseudo2)) {
-            $erreurs[] = 'Utilisateur inconnu ou mot de passe incorrect !';
-        }
-    }
-
-        // Libération de la mémoire associée au résultat de la requête
-    mysqli_free_result($res);
-        */
-    
-
-
+    // Libération de la mémoire associée au résultat de la requête
+    mysqli_free_result($resultat);
 
     // si erreurs --> retour
     if (count($erreurs) > 0) {
@@ -178,13 +140,12 @@ $pseudo = mysqli_real_escape_string($bd, $_POST['pseudo']);
 
     // mémorisation du pseudo et redacteur dans une variable de session 
 
-    $_SESSION['pseudo'] = $pseudo;
-    $_SESSION['redacteur'] = $utRedacteur;
+    $_SESSION['pseudo'] = $donneesUtilisateur['utPseudo'];
+    $_SESSION['redacteur'] = $donneesUtilisateur['utRedacteur'];
     
-    return $erreurs;
     // redirection vers la page précédente ou index.php !
-    /*
-    if(empty($_SERVER['HTTP_REFERER']) || strpos($_SERVER['HTTP_REFERER'], 'connexion.php') !== false) {
+
+    if(empty($_SERVER['HTTP_REFERER']) || strpos($_SERVER['HTTP_REFERER'], 'http://localhost:8080/gazette/php/connexion.php') !== false) {
         header('Location: ../index.php'); // Si la page précédente n'existe pas ou est connexion.php, on redirige vers index.php
     } else {
         $referer = $_SERVER['HTTP_REFERER'];
@@ -192,5 +153,5 @@ $pseudo = mysqli_real_escape_string($bd, $_POST['pseudo']);
     }
 
     exit(); //===> Fin du script
-    */
+    
 }
